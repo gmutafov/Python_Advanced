c6dde3cca48f481ab4a3e9b0438507cefd20fdb6
gmutafov <georgimutafovbg@gmail.com>
2024-02-04 13:56:07 +0200

    Exercises

--- Python_Advanced/Wokshop_Lab/workshop.py
+++ Python_Advanced/Wokshop_Lab/workshop.py
@@ -9,6 +9,39 @@ class InvalidColumnChoice(Exception):
 ROWS = 6
 COLS = 7
 
+DIRECTIONS = {
+    'left': (0, -1),
+    'up': (-1, 0),
+    'main_diagonal': (-1, -1),
+    'other_diagonal': (-1, 1),
+}
+
+
+def travel_direction(coordinates, curr_row, curr_col,  el, board):
+    count = 0
+    for i in range(1, 4):
+        row_direction, col_direction = coordinates
+        next_el_row_idx = curr_row + row_direction * i
+        next_el_col_idx = curr_col + col_direction * i
+    try:
+        if board[next_el_row_idx][next_el_col_idx] == el:
+            count += 1
+        else:
+            return count
+    except IndexError:
+        return count
+
+
+def travel_opposite_direction(coords, curr_row, curr_col, searched_el, board):
+
+
+
+def is_winner(curr_row, curr_col, board):
+    for direction, coords in DIRECTIONS.items():
+        searched_el = board[curr_row][curr_col]
+        travel_direct_count = travel_direction(coords, curr_row, curr_col, searched_el, board)
+        travel_opposite_direct_count = travel_opposite_direction(coords, curr_row, curr_col, searched_el, board)
+
 
 def print_matrix(board):
     for row in board:
@@ -44,7 +77,8 @@ while True:
         column_index = int(column) - 1
         row = getting_first_row_index(column_index, board)
         board[row][column_index] = player
-
+        if is_winner(row, column_index, board):
+            break
     except FullColumnError as ex:
         print(str(ex))
         continue
@@ -54,4 +88,5 @@ while True:
 
     print_matrix(board)
     turns += 1
-
+print(f'WINNER is Player {player}')
+print_matrix(board)
