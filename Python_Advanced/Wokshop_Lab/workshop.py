3f14fabc86e3f5bf1fb7d3c666b0dedf7c2ac444
gmutafov <georgimutafovbg@gmail.com>
2024-02-04 16:38:31 +0200

    Exercises

--- Python_Advanced/Wokshop_Lab/workshop.py
+++ Python_Advanced/Wokshop_Lab/workshop.py
@@ -9,6 +9,7 @@ class InvalidColumnChoice(Exception):
 ROWS = 6
 COLS = 7
 
+
 DIRECTIONS = {
     'left': (0, -1),
     'up': (-1, 0),
@@ -17,49 +18,64 @@ DIRECTIONS = {
 }
 
 
-def travel_direction(coordinates, curr_row, curr_col,  el, board):
+def travel_direction(coordinates, curr_row, curr_col, element, board):
     count = 0
+    row_direct, col_direct = coordinates
     for i in range(1, 4):
-        row_direction, col_direction = coordinates
-        next_el_row_idx = curr_row + row_direction * i
-        next_el_col_idx = curr_col + col_direction * i
-    try:
-        if board[next_el_row_idx][next_el_col_idx] == el:
-            count += 1
-        else:
+        next_el_row_index = curr_row + row_direct * i
+        next_el_col_index = curr_col + col_direct * i
+        try:
+            if board[next_el_row_index][next_el_col_index] == element:
+                count += 1
+            else:
+                return count
+        except IndexError:
             return count
-    except IndexError:
-        return count
 
 
-def travel_opposite_direction(coords, curr_row, curr_col, searched_el, board):
+def travel_opposite_direction(coordinates, curr_row, curr_col, element, board):
+    count = 0
+    row_direct, col_direct = coordinates
+    for i in range(1, 4):
+        next_el_row_index = curr_row - row_direct * i
+        next_el_col_index = curr_col - col_direct * i
+        try:
+            if board[next_el_row_index][next_el_col_index] == element:
+                count += 1
+            else:
+                return count
+        except IndexError:
+            return count
 
 
-
-def is_winner(curr_row, curr_col, board):
+def is_winner(row_index, col_index, board):
     for direction, coords in DIRECTIONS.items():
-        searched_el = board[curr_row][curr_col]
-        travel_direct_count = travel_direction(coords, curr_row, curr_col, searched_el, board)
-        travel_opposite_direct_count = travel_opposite_direction(coords, curr_row, curr_col, searched_el, board)
+        searched_el = board[row_index][col_index]
+        travel_direction_count = travel_direction(coords, row_index, col_index, searched_el, board)
+        travel_opposite_direction_count = travel_opposite_direction(coords, row_index, col_index, searched_el, board)
+        if travel_direction_count + travel_opposite_direction_count + 1 >= 4:
+            return True
+    else:
+        return False
 
 
-def print_matrix(board):
+def print_board(board):
     for row in board:
         print(row)
 
 
-def getting_first_row_index(col_index, board):
-    for row_index in range(ROWS-1, -1, -1):
+def validate_column_choice(col):
+    if 1 <= col <= COLS:
+        return True
+    raise InvalidColumnChoice
+
+
+def get_first_available_row(col_index, board):
+    for row_index in range(ROWS - 1, -1, -1):
         if board[row_index][col_index] == 0:
             return row_index
     else:
-        raise FullColumnError('This column is full, please select another one')
-
-
-def validate_col_choice(col):
-    if 1 <= col <= COLS:
-        return True
-    raise InvalidColumnChoice()
+        raise FullColumnError
 
 
 board = []
@@ -68,25 +84,27 @@ for _ in range(ROWS):
     board.append([0 for _ in range(COLS)])
 
 turns = 1
+
 while True:
     player = 2 if turns % 2 == 0 else 1
 
     try:
-        column = int(input(f'Player {player}, please choose a column'))
-        validate_col_choice(column)
+        column = int(input(f"Player {player}, please choose a column: "))
+        validate_column_choice(column)
         column_index = int(column) - 1
-        row = getting_first_row_index(column_index, board)
+        row = get_first_available_row(column_index, board)
         board[row][column_index] = player
         if is_winner(row, column_index, board):
             break
     except FullColumnError as ex:
-        print(str(ex))
+        print(f"This column is full, please select another one")
         continue
     except (InvalidColumnChoice, ValueError):
-        print(f'This column is invalid, please select a number between 1 and {COLS}')
+        print(f"This column is invalid, please select a number between 1 and {COLS}")
         continue
 
-    print_matrix(board)
+    print_board(board)
     turns += 1
-print(f'WINNER is Player {player}')
-print_matrix(board)
+
+print_board(board)
+print(f"WINNER is Player {player}")
