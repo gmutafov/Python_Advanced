171705eabbe9fc53fdec852dbcc067558fb01ba0
gmutafov <georgimutafovbg@gmail.com>
2024-02-06 17:41:30 +0200

    Exercises

--- Python_Advanced/Wokshop_Lab/workshop.py
+++ Python_Advanced/Wokshop_Lab/workshop.py
@@ -8,56 +8,45 @@ class InvalidColumnChoice(Exception):
 
 ROWS = 6
 COLS = 7
+MAXIMUM_CONNECTIONS = 4
 
 
-DIRECTIONS = {
-    'left': (0, -1),
-    'up': (-1, 0),
-    'main_diagonal': (-1, -1),
-    'other_diagonal': (-1, 1),
+DIRECTION_MAPPER = {
+    "left": (0, -1),
+    "up": (-1, 0),
+    "main_diagonal": (-1, -1),
+    "other_diagonal": (-1, 1),
 }
 
 
-def travel_direction(coordinates, curr_row, curr_col, element, board):
+def travel_direction(coordinates, current_row, current_col, element, sign, board):
     count = 0
-    row_direct, col_direct = coordinates
-    for i in range(1, 4):
-        next_el_row_index = curr_row + row_direct * i
-        next_el_col_index = curr_col + col_direct * i
+    row_direction, col_direction = coordinates
+
+    for i in range(1, MAXIMUM_CONNECTIONS):
+        next_element_row_index = eval(f"{current_row} {sign} {row_direction} * {i}")
+        next_element_col_index = eval(f"{current_col} {sign} {col_direction} * {i}")
+
         try:
-            if board[next_el_row_index][next_el_col_index] == element:
+            if board[next_element_row_index][next_element_col_index] == element:
                 count += 1
             else:
                 return count
         except IndexError:
             return count
+    return count
 
 
-def travel_opposite_direction(coordinates, curr_row, curr_col, element, board):
-    count = 0
-    row_direct, col_direct = coordinates
-    for i in range(1, 4):
-        next_el_row_index = curr_row - row_direct * i
-        next_el_col_index = curr_col - col_direct * i
-        try:
-            if board[next_el_row_index][next_el_col_index] == element:
-                count += 1
-            else:
-                return count
-        except IndexError:
-            return count
+def is_winner(current_row_index, current_col_index, board):
+    for direction, coords in DIRECTION_MAPPER.items():
+        searched_element = board[current_row_index][current_col_index]
+        travel_direction_count = travel_direction(coords, current_row_index, current_col_index, searched_element, "+", board)
+        travel_opposite_direction_count = travel_direction(coords, current_row_index, current_col_index, searched_element, "-", board)
 
-
-def is_winner(row_index, col_index, board):
-    for direction, coords in DIRECTIONS.items():
-        searched_el = board[row_index][col_index]
-        travel_direction_count = travel_direction(coords, row_index, col_index, searched_el, board)
-        travel_opposite_direction_count = travel_opposite_direction(coords, row_index, col_index, searched_el, board)
-        if travel_direction_count + travel_opposite_direction_count + 1 >= 4:
+        if travel_direction_count + travel_opposite_direction_count + 1 >= MAXIMUM_CONNECTIONS:
             return True
     else:
         return False
-
 
 def print_board(board):
     for row in board:
@@ -71,11 +60,15 @@ def validate_column_choice(col):
 
 
 def get_first_available_row(col_index, board):
-    for row_index in range(ROWS - 1, -1, -1):
+    for row_index in range(ROWS-1, -1, -1):
         if board[row_index][col_index] == 0:
             return row_index
     else:
         raise FullColumnError
+
+
+def is_board_full(turns):
+    return ROWS*COLS <= turns
 
 
 board = []
@@ -94,8 +87,12 @@ while True:
         column_index = int(column) - 1
         row = get_first_available_row(column_index, board)
         board[row][column_index] = player
+        turns += 1
         if is_winner(row, column_index, board):
             break
+        if is_board_full(turns):
+            print("Board is full, nobody wins")
+            exit(0)
     except FullColumnError as ex:
         print(f"This column is full, please select another one")
         continue
@@ -104,7 +101,7 @@ while True:
         continue
 
     print_board(board)
-    turns += 1
+
 
 print_board(board)
-print(f"WINNER is Player {player}")
+print(f"WINNER is player {player}")
