91294fb8712bb6d5f4c321cd812720c596557c6b
gmutafov <georgimutafovbg@gmail.com>
2024-03-01 14:54:40 +0200

    Exercises

--- Python_OOP/Classes_and_Objects_Exercise/Guild_System/project/guild.py
+++ Python_OOP/Classes_and_Objects_Exercise/Guild_System/project/guild.py
@@ -1,35 +1,37 @@
-from typing import List
-
 from project import player
 from project.player import Player
 
 
 class Guild:
-    def __init__(self, name):
+
+    def __init__(self, name: str):
         self.name = name
-        self.players: List[Player] = []
+        self.players = []
 
     def assign_player(self, player: Player):
         if player in self.players:
-            return f"Player {player} is already in the guild."
-        if player != player.guild:
-            return f"Player {player} is in another guild."
+            return f"Player {player.name} is already in the guild."
 
-        self.players.append(player)
+        if player.guild != player.DEFAULT_GUILD:
+            return f"Player {player.name} is in another guild."
+
         player.guild = self.name
+        self.players.append(player)
+        return f"Welcome player {player.name} to the guild {self.name}"
 
-    def kick_player(self, player_name):
-        if player_name not in self.players:
-            return f"Player {player_name} is not in the guild."
+    def kick_player(self, player_name: str):
+        for player in self.players:
+            if player.name == player_name:
+                player.guild = player.DEFAULT_GUILD
+                self.players.remove(player)
+                return f"Player {player_name} has been removed from the guild."
 
-        self.players.remove(player_name)
-        player.guild = Player.DEFAULT_GUILD
-        return f"Player {player_name} has been removed from the guild"
+        return f"Player {player_name} is not in the guild."
 
     def guild_info(self):
-        result = f"Guild; {self.name}"
+        result = f"Guild: {self.name}"
 
-        for p in self.players:
-            result += f"\n{p.player_info()}"
+        for player in self.players:
+            result += f"\n{player.player_info()}"
 
         return result
--- Python_OOP/Classes_and_Objects_Exercise/Guild_System/project/player.py
+++ Python_OOP/Classes_and_Objects_Exercise/Guild_System/project/player.py
@@ -12,10 +12,8 @@ class Player:
         if skill_name in self.skills:
             return "Skill already added"
         self.skills[skill_name] = mana_cost
-        return f"{skill_name} added to the collection of the player {self.name}"
+        return f"Skill {skill_name} added to the collection of the player {self.name}"
 
     def player_info(self):
-        skills_result = '\n'.join(f"=== {skill} - {mana}" for skill, mana in self.skills.items())
-        return (f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\n"
-                f"MP: {self.mp}\nSkills: {self.skills}\n"
-                f"{skills_result}")
+        skills_result = '\n'.join(f"==={skill} - {mana}" for skill, mana in self.skills.items())
+        return f"Name: {self.name}\nGuild: {self.guild}\nHP: {self.hp}\nMP: {self.mp}\n{skills_result}\n"
