6573f8849056a42aea01da4514433e02a72f2a22
gmutafov <georgimutafovbg@gmail.com>
2024-03-13 16:06:52 +0200

    .

--- Python_OOP/Polymorphism_and_Abstraction_Exercise/Wild_Farm/project/animals/birds.py
+++ Python_OOP/Polymorphism_and_Abstraction_Exercise/Wild_Farm/project/animals/birds.py
@@ -1,14 +1,34 @@
+from typing import List, Type
+
 from project.animals.animal import Bird
+from project.food import Food, Meat, Vegetable, Fruit, Seed
 
 
 class Owl(Bird):
 
-    def make_sound(self):
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.25
+
+    @staticmethod
+    def make_sound() -> str:
         return "Hoot Hoot"
 
 
 class Hen(Bird):
-    def make_sound(self):
+
+    @property
+    def food_that_eats(self):
+        return [Meat, Fruit, Vegetable, Seed]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.35
+
+    @staticmethod
+    def make_sound() -> str:
         return "Cluck"
-
-
--- Python_OOP/Polymorphism_and_Abstraction_Exercise/Wild_Farm/project/animals/mammals.py
+++ Python_OOP/Polymorphism_and_Abstraction_Exercise/Wild_Farm/project/animals/mammals.py
@@ -1,21 +1,62 @@
 from project.animals.animal import Mammal
+from project.food import Meat, Fruit, Vegetable, Seed
 
 
 class Mouse(Mammal):
-    def make_sound(self):
+
+    @property
+    def food_that_eats(self):
+        return [Fruit, Vegetable]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.10
+
+    @staticmethod
+    def make_sound() -> str:
         return "Squeak"
 
 
 class Dog(Mammal):
-    def make_sound(self):
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.40
+
+    @staticmethod
+    def make_sound() -> str:
         return "Woof!"
 
 
 class Cat(Mammal):
-    def make_sound(self):
+
+    @property
+    def food_that_eats(self):
+        return [Meat, Vegetable]
+
+    @property
+    def gained_weight(self) -> float:
+        return 0.30
+
+    @staticmethod
+    def make_sound() -> str:
         return "Meow"
 
 
 class Tiger(Mammal):
-    def make_sound(self):
+
+    @property
+    def food_that_eats(self):
+        return [Meat]
+
+    @property
+    def gained_weight(self) -> float:
+        return 1.00
+
+    @staticmethod
+    def make_sound() -> str:
         return "ROAR!!!"
