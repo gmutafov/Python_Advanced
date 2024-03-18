3b0a9fe3f7b39e410354547c5894ea8503fd0de8
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-03-18 08:01:22 +0200

    Exercises

---
+++ Python_OOP/Iterators_and_Generators_Exercise/take_skip.py
@@ -0,0 +1,25 @@
+class take_skip:
+    def __init__(self, step, count):
+        self.step = step
+        self.count = count
+        self.current = 0
+        self.generated = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.generated >= self.count:
+            raise StopIteration
+        else:
+            result = self.current
+            self.current += self.step
+            self.generated += 1
+            return result
+
+
+numbers = take_skip(2, 6)
+for number in numbers:
+    print(number)
+
+
